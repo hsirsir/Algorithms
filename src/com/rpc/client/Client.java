dd3cd660d18dd7d62243302983d0002ee7fc7867
hsirsir <13717542974@163.com>
2017-05-02 13:26:28 +0800

    add rpc code

---
+++ src/com/rpc/client/Client.java
@@ -0,0 +1,7 @@
+package com.rpc.client;
+
+/**
+ * Created by 6estates on 02-May-17.
+ */
+public class Client {
+}
---
+++ src/com/rpc/server/Server.java
@@ -0,0 +1,7 @@
+package com.rpc.server;
+
+/**
+ * Created by 6estates on 02-May-17.
+ */
+public class Server {
+}
